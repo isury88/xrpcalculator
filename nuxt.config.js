e188f7de492852c42c4962d2dd6e09aaba2fd854
isury88 <isury@hotmail.it>
2020-08-23 19:52:26 +0100

    deleting base url

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,10 +9,7 @@ export default {
   ** Nuxt target
   ** See https://nuxtjs.org/api/configuration-target
   */
-  target: 'static',
-  router: {
-    base: 'xrpcalculator'
-  },
+  target: 'static'
   /*
   ** Headers of the page
   ** See https://nuxtjs.org/api/configuration-head

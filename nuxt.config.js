c6f034143211034517ad8bf7797cacbd57cfca5b
isury88 <isury@hotmail.it>
2020-09-10 08:05:07 +0100

    base route updated

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,9 +9,9 @@ export default {
    ** See https://nuxtjs.org/api/configuration-target
    */
   target: "static",
-  // router: {
-  //   base: "/xrpprofitcalculators/"
-  // },
+  router: {
+    base: "/"
+  },
   /*
    ** Headers of the page
    ** See https://nuxtjs.org/api/configuration-head

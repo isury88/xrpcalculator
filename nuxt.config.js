848bab767d32a68d0eed7c7745759f167eb827d2
isury88 <isury@hotmail.it>
2020-09-10 08:34:53 +0100

    GA added

--- nuxt.config.js
+++ nuxt.config.js
@@ -41,7 +41,7 @@ export default {
    ** Plugins to load before mounting the App
    ** https://nuxtjs.org/guide/plugins
    */
-  plugins: ["~plugins/filters.js"],
+  plugins: ["~plugins/filters.js", { src: "~plugins/ga.js", mode: "client" }],
   /*
    ** Auto import components
    ** See https://nuxtjs.org/api/configuration-components
